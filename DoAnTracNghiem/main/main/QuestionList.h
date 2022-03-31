240fe8b0f6f3434b62daedd54cc6ef640f59f958
dhuynguyen2002 <dhuynguyen2002@users.noreply.github.com>
2022-04-01 00:59:48 +0700

    update
    --của Danh ăn bug kìa

--- DoAnTracNghiem/main/main/QuestionList.h
+++ DoAnTracNghiem/main/main/QuestionList.h
@@ -1,108 +1,31 @@
-#include "QuestionList.h"
+#pragma once
+#include "Question.h"
 
-QuestionList::QuestionList(int capacity) {
-	this->capacity = capacity;
-	list = new Question * [capacity];
-}
+class QuestionList
+{
+private:
+	int size = 0;
+	int capacity = 0;
+	Question** list = nullptr;
+public:
+	QuestionList(int capacity);
 
-int QuestionList::getSize() {
-	return this->size;
-}
+	bool IsFull();
+	bool IsEmpty();
+	void Clear();
 
-int QuestionList::getCapacity() {
-	return this->capacity;
-}
+	bool PushBack(Question* question);
+	bool PopBack();
+	bool InsertAt(Question* question, int position);
+	bool DeleteAt(int position);
 
-bool QuestionList::IsFull() {
-	return size == capacity;
-}
+	int getSize();
+	int getCapacity();
 
-bool QuestionList::IsEmpty() {
-	return size == 0;
-}
+	void setCapacity(int capacity);
 
-void QuestionList::Clear() {
-	for (int i = 0; i < size; ++i) {
-		delete list[i];
-	}
+	Question** getList() const;
+	QuestionList();
+	~QuestionList();
+};
 
-	delete[] list;
-	size = 0;
-}
-
-bool QuestionList::PushBack(Question* question) {
-	if (IsFull()) {
-		return 0;
-	}
-
-	list[size] = new Question(question);
-	size++;
-
-	return 1;
-}
-
-bool QuestionList::PopBack() {
-	if (size == 0) {
-		return 0;
-	}
-
-	delete list[size - 1];
-	size--;
-}
-
-bool QuestionList::InsertAt(Question* question, int position) {
-	if (IsFull()) {
-		return 0;
-	}
-
-	if (position < 0 || position > size) {
-		return 0;
-	}
-
-	for (int i = size - 1; i >= position; --i) {
-		list[i + 1] = list[i];
-	}
-
-	size++;
-	list[position] = question;
-	return 1;
-}
-
-bool QuestionList::DeleteAt(int position) {
-	if (IsEmpty()) {
-		return 0;
-	}
-
-	if (position < 0 || position > size) {
-		return 0;
-	}
-
-	for (int i = position; i < size - 1; ++i) {
-		list[i] = list[i + 1];
-	}
-
-	size--;
-	return 0;
-}
-
-void QuestionList::setCapacity(int capacity) {
-	while (capacity < this->size) {
-		PopBack();
-	}
-}
-
-Question** QuestionList::getList() const {
-	return list;
-}
-
-QuestionList::QuestionList() {
-}
-
-QuestionList::~QuestionList() {
-
-	for (int i = 0; i < size; ++i) {
-		delete list[i];
-	}
-
-	delete[]list;
-}
